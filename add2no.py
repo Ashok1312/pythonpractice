5057afbccff1ae3f6c968c6d73bb98f45c5b3fbc
Ashok1312 <ashok.mishra1320@gmail.com>
2019-01-08 16:14:48 +0530

    this program will add two numbers

---
+++ add2no.py
@@ -0,0 +1,5 @@
+# add 2 given number
+first_No_to_Add = int(input("enter first number: "))
+second_No_to_Add = int(input("enter second number: "))
+sum_Of_TwoNo = first_No_to_Add + second_No_to_Add
+print("sum of two number is: ", sum_Of_TwoNo)
