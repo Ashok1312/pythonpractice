0e788bbd2d07aa0c8299503c58f6375a3ff26cc2
Ashok1312 <ashok.mishra1320@gmail.com>
2019-01-14 11:58:58 +0530

    python program to import data in mysql database

---
+++ importdb.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, template_folder='render html')
+
+# config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'FLASKAPP'
+
+
+mysql = MySQL(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(NAME, EMAIL) VALUES(%s, %s)", (name, email))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index1.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
